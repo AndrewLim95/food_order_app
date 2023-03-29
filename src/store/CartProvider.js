2f85f8760044387592c3dc9ac2bebe7d3e16ef27
andrew <andrew0@live.com.sg>
2023-03-29 23:24:18 +0800

    able to add to cart number

---
+++ src/store/CartProvider.js
@@ -0,0 +1,55 @@
+import CartContext from "./cart-context";
+import { useReducer } from "react";
+
+const defaultCartState = {
+    items: [],
+    totalAmount: 0
+};
+
+const cartReducer = (state, action) => {
+    if (action.type === 'ADD_CART_ITEM'){
+        const updateItems = state.items.concat(action.item);
+        const updateTotalAmount = state.totalAmount + action.item.price*action.item.amount;
+        return {
+            items: updateItems,
+            totalAmount: updateTotalAmount 
+        }
+    }
+    return (
+        defaultCartState
+    );
+};
+
+const CartProvider = (props) => {
+    
+    const [cartState, dispatchCartAction] = useReducer(cartReducer,defaultCartState);
+
+    const addItemToCartHandler = (item)=> {
+        dispatchCartAction({
+            type: 'ADD_CART_ITEM',
+            item: item
+        });
+    };
+    const removeItemToCartHandler = (id)=> {
+        dispatchCartAction({
+            type: 'REMOVE_CART_ITEM',
+            id: id
+        })
+    };
+
+
+    const cartContext = {
+        items: cartState.items,
+        totalAmount: cartState.totalAmount,
+        addItem: addItemToCartHandler,
+        removeItem: removeItemToCartHandler
+    };
+
+    return(
+        <CartContext.Provider value={cartContext}>
+        {props.children}
+        </CartContext.Provider>
+    )
+}
+
+export default CartProvider;
